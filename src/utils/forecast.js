60234cd54b75fd42465b90ca2108c33e72b99ee5
DiogoMedina97 <diogo_medina97@hotmail.com>
2020-04-19 23:31:19 -0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+const config = require('../config/config.json')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=' + config.WEATHERSTACK_API_KEY + '&query=' + latitude + ',' + longitude + '&units=f';
+
+    request({ url, json: true }, (error, { body }) => {
+        if(error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if(body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out. The humidity is ' + body.current.humidity + '%.')
+        }
+    })
+}
+
+module.exports = forecast
